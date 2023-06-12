10e835024c670429c1689f3f8a42ae7f424f3429
EvATive7 <evative7@gmail.com>
2023-06-12 13:32:45 +0800

    dev impl adb  dev disposed

--- src/ADBSharp/Device/ADBDevice.cs
+++ src/ADBSharp/Device/ADBDevice.cs
@@ -6,14 +6,33 @@ using System.Threading.Tasks;
 
 namespace ADBSharp.Device
 {
-    public class ADBDevice
+    public class ADBDevice:IDisposable
     {
+        private bool disposedValue;
+
         public string Name { get; set; }
         public string Status { get; set; } = "";
 
         public ADBDevice(string name)
         {
             Name = name;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    Status = "disconnected";
+                }
+                disposedValue = true;
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
     }
 }
